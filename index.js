f90ee23bc4f19c16c1abff05d0c9e388dac9fee7
Gonçalo Amaro <samaro.dev@gmail.com>
2025-02-12 18:34:11 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+/* ShowMore button */
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = ">";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "<";
+      moreText.style.display = "inline";
+    }
+  }
